9926685c319f8ba39e17523850f97a916117ff8f
Tiago Castro Henriques <trinosauro@gmail.com>
2017-05-31 22:28:02 +0200

    Put calculations and output in different functions

--- paper-scissors-rock-text/main.js
+++ paper-scissors-rock-text/main.js
@@ -1,60 +1,43 @@
-let outputResult = function (list, options, humanChoice, computerChoice) {
-    list.innerHTML = list.innerHTML + '<li>computer chose: ' + computerChoice + '</li>'
-    // list.innerHTML = list.innerHTML + `<li>computer chose: ${computerChoice} </li>`
+let computeResult = function(humanChoice) {
+    let result
+    let options = ['ROCK', 'SCISSORS', 'PAPER']
 
-    if (options.includes(humanChoice)) {
-        if (humanChoice === computerChoice) {
-            list.innerHTML = list.innerHTML + '<li>it\'s a draw!</li>'
-        } else {
-            if (humanChoice === 'ROCK') {
-                if (computerChoice === 'SCISSORS') {
-                    humanWins = true
-                } else {
-                    humanWins = false
-                }
-            } else if (humanChoice === 'SCISSORS') {
-                if (computerChoice === 'PAPER') {
-                    humanWins = true
-                } else {
-                    humanWins = false
-                }
-            } else if (humanChoice === 'PAPER') {
-                if (computerChoice === 'ROCK') {
-                    humanWins = true
-                } else {
-                    humanWins = false
-                }
-            }
-
-            if (humanWins) {
-                list.innerHTML = list.innerHTML + '<li>you win!</li>'
-            } else {
-                list.innerHTML = list.innerHTML + '<li>you lose!</li>'
-            }
-        }
+    let r0 = Math.random()
+    let r1 = r0 * 3
+    let computerIndex = Math.floor(r1)
+    let humanIndex = options.indexOf(humanChoice)
+    console.log(`human index: ${humanIndex}; computer index: ${computerIndex}`)
+    // the line above is using something cool from ES6: template strings
+    // please note that ` backquote is NOT the same as single-quote '
+    if (humanIndex === computerIndex) {
+        result = 0 
     } else {
-        list.innerHTML = list.innerHTML + '<li>you lose! Choose a valid choice, loser</li>'
+        let indexAfterHumanIndex = (humanIndex + 1) % 3
+        if (computerIndex === indexAfterHumanIndex) {
+            result = 1
+        } else {
+            result = -1
+        }
+    }
+    return result
+}
+
+let outputResult = function(result) {
+    let listElement = document.querySelectorAll("#listElement")[0]
+
+    if (result === -1) {
+        listElement.innerHTML = listElement.innerHTML + '<li>you lose!</li>'
+    } else if (result === 0) {
+        listElement.innerHTML += '<li>it\'s a draw...</li>'
+    } else if (result === 1) {
+        listElement.innerHTML += '<li>you win!</li>'
+    } else {
+        // error! Should never get here
     }
 }
 
 let play = function () {
-    let computerChoice
-    let choicesSelected = false
-    let humanChoice
-    let result
-    let r0
-    let r1
-    let r2
-    let options
-    let list = document.querySelectorAll("#list")[0]
-    humanChoice = prompt('Choose rock, paper or scissors!').toUpperCase()
-
-    r0 = Math.random()
-    r1 = r0 * 3
-    r2 = Math.floor(r1)
-
-    options = ['ROCK', 'PAPER', 'SCISSORS']
-    computerChoice = options[r2]
-
-    outputResult(list, options, humanChoice, computerChoice)
+    let humanChoice = prompt('Choose rock, paper or scissors!').toUpperCase()
+    let result = computeResult(humanChoice)
+    outputResult(result)
 }
