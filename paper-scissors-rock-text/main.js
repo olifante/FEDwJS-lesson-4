2876ebb0e98f5785275a08b4193ba51e0a6a3d65
Tiago Castro Henriques <trinosauro@gmail.com>
2017-05-31 23:36:36 +0200

    Add some comments

--- paper-scissors-rock-text/main.js
+++ paper-scissors-rock-text/main.js
@@ -1,22 +1,35 @@
 let computeResult = function(humanChoice) {
-    let result
     let options = ['ROCK', 'SCISSORS', 'PAPER']
+    // the options are "well-ordered", meaning that
+    // each element "wins" over the next element
+    // symbolically, ROCK > SCISSORS > PAPER ( > ROCK > ... > ... )
 
-    let r0 = Math.random()
-    let r1 = r0 * 3
-    let computerIndex = Math.floor(r1)
+    let r0 = Math.random() // r0 is a real number between 0 and 1
+    let r1 = r0 * 3 // r1 is a real number between 0 and 3
+    let computerIndex = Math.floor(r1) // floor rounds real numbers down 
     let humanIndex = options.indexOf(humanChoice)
+
     console.log(`human index: ${humanIndex}; computer index: ${computerIndex}`)
     // the line above is using something cool from ES6: template strings
-    // please note that ` backquote is NOT the same as single-quote '
+    // please note that backquote (`) is NOT the same as single-quote (')
+
+    let result
     if (humanIndex === computerIndex) {
-        result = 0 
+        result = 0
     } else {
         let indexAfterHumanIndex = (humanIndex + 1) % 3
+        // the above line uses the remainder operator.
+        // It's actually something you learned in primary school,
+        // when studying imperfect division. Some examples:
+        // 7 % 3 === 1
+        // 10 % 5 === 0
+        // 10 % 3 === 1
+
         if (computerIndex === indexAfterHumanIndex) {
             result = 1
         } else {
             result = -1
+            // if it's not a draw or a victory, defeat is only option left
         }
     }
     return result
@@ -24,6 +37,12 @@ let computeResult = function(humanChoice) {
 
 let outputResult = function(result) {
     let listElement = document.querySelectorAll("#listElement")[0]
+    // querySelectorAll accepts standard CSS selector, e.g.
+    // an element: document.querySelectorAll('h1')
+    // a class: document.querySelectorAll('.someClass')
+    // an ID: document.querySelectorAll('#someId')
+    // it always returns an array, which for IDs should only
+    // contain ONE element.
 
     if (result === -1) {
         listElement.innerHTML = listElement.innerHTML + '<li>you lose!</li>'
@@ -32,8 +51,8 @@ let outputResult = function(result) {
     } else if (result === 1) {
         listElement.innerHTML += '<li>you win!</li>'
     } else {
-        // error! Should never get here
-    }
+        console.log('Invalid input. Result should be -1, 0 or 1')
+   }
 }
 
 let play = function () {
